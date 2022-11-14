9f2e2c9888a5cc6a1458f1ce4cd25cf102e067f6
Adrian Butler Jr <butlerjr123@gmail.com>
2022-11-14 14:16:48 -0600

    added methods to ApplicationUser

--- src/main/java/adrian/codefellowship/models/ApplicationUser.java
+++ src/main/java/adrian/codefellowship/models/ApplicationUser.java
@@ -1,28 +1,82 @@
 package adrian.codefellowship.models;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity
-public class ApplicationUser
+public class ApplicationUser implements UserDetails
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	Long id;
 
 	private String username;
 	private String password;
 	private String firstName;
 	private Date dateOfBirth;
 
+	public ApplicationUser(String username, String password, String firstName, Date dateOfBirth)
+	{
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public ApplicationUser(String username, String password, String firstName)
+	{
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+	}
+
+	public ApplicationUser()
+	{
+
+	}
+
 	public String getUsername()
 	{
 		return username;
 	}
 
+	@Override
+	public boolean isAccountNonExpired()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled()
+	{
+		return true;
+	}
+
 	public void setUsername(String username)
 	{
 		this.username = username;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities()
+	{
+		return null;
 	}
 
 	public String getPassword()
